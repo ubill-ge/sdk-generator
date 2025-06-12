5ec4e80b60abe23467fbfdbcb2b28fed606c197c
ABGEO <takalandzet@gmail.com>
2025-06-12 18:23:26 +0400

    ci: integrate release-drafter

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,19 +6,18 @@ GENERATOR=$1
 OUT_DIR=$2
 TEMPLATES_DIR=$(mktemp -d)
 
-cp -r ./templates/${GENERATOR} ${TEMPLATES_DIR}/${GENERATOR}
-cp -r ./templates/base/* ${TEMPLATES_DIR}/${GENERATOR}
-
-mkdir -p ${TEMPLATES_DIR}/${GENERATOR}/.github
-cp .github/CODEOWNERS ${TEMPLATES_DIR}/${GENERATOR}/.github
-cp LICENSE ${TEMPLATES_DIR}/${GENERATOR}
+mkdir -p "${TEMPLATES_DIR}/.github"
+rsync -a "./templates/base/" "${TEMPLATES_DIR}/"
+cp .github/CODEOWNERS "${TEMPLATES_DIR}/.github"
+cp LICENSE "${TEMPLATES_DIR}"
+rsync -a "./templates/${GENERATOR}/" "${TEMPLATES_DIR}/"
 
 openapi-generator-cli generate \
   -i spec/openapi.yaml \
   -g ${GENERATOR} \
   -o ${OUT_DIR} \
   -c config/${GENERATOR}.yaml \
-  -t ${TEMPLATES_DIR}/${GENERATOR} \
+  -t ${TEMPLATES_DIR} \
   --enable-post-process-file
 
 rm -rf $TEMPLATES_DIR
